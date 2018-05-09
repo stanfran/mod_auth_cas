7d835d35a2708cb8fc0187683ea499e961824a97
Ned Regina <stanfran@users.noreply.github.com>
2018-05-09 10:04:12 -0400

    trying HTTP_OK again

--- src/mod_auth_cas.c
+++ src/mod_auth_cas.c
@@ -667,7 +667,7 @@ int redirectRequest(request_rec *r, cas_cfg *c)
 		if(c->CASDebug)
 			ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, "Adding Cas-Status: Authenticate, Cas-Authenticate: %s", destination);
 
-		return HTTP_UNAUTHORIZED;
+		return HTTP_OK;
 	}
 
 	// Redirect normally
